5639cca833219914a4c748c499877d95b5c71808
Felipe Rodrigues <felipe.git@outlook.com>
2025-03-25 22:01:52 -0300

    Feat: Products categories, brands and serial numbers filter

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
+from categories.models import Category
+from brands.models import Brand
 
 
 class ProductListView(ListView):
@@ -12,11 +14,29 @@ class ProductListView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         name = self.request.GET.get('name')
+        category = self.request.GET.get('category')
+        brand = self.request.GET.get('brand')
+        serial_number = self.request.GET.get('serial_number')
 
         if name:
             queryset = queryset.filter(name__icontains=name)
 
+        if category:
+            queryset = queryset.filter(category__id=category)
+
+        if brand:
+            queryset = queryset.filter(brand__id=brand)
+
+        if serial_number:
+            queryset = queryset.filter(serial_number__icontains=serial_number)
+
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['brands'] = Brand.objects.all()
+        return context
 
 
 class ProductCreateView(CreateView):
