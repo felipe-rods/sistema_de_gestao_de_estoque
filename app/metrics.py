a71a4461608c8a8c7600b615a02a7ee4e6941056
Felipe Rodrigues <felipe.git@outlook.com>
2025-04-11 19:09:35 -0300

    Fix: Product graphic bug

--- app/metrics.py
+++ app/metrics.py
@@ -73,11 +73,11 @@ def get_daily_sales_quantity_data():
     )
 
 
-def graphic_product_category_metric():
+def get_graphic_product_category_metric():
     categories = Category.objects.all()
     return {category.name: Product.objects.filter(category=category).count() for category in categories}
 
 
-def graphic_product_brand_metric():
+def get_graphic_product_brand_metric():
     brands = Brand.objects.all()
     return {brand.name: Product.objects.filter(brand=brand).count() for brand in brands}
