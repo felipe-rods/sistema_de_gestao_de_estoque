cdf593074f85a8fed260d9c6c4e8a49f8dedebe2
Felipe Rodrigues <felipe.git@outlook.com>
2025-04-27 19:33:50 -0300

    Feat: All API domains crud

--- inflows/urls.py
+++ inflows/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('inflows/list/', views.InflowListView.as_view(), name='inflow_list'),
     path('inflows/create/', views.InflowCreateView.as_view(), name='inflow_create'),
     path('inflows/<int:pk>/detail/', views.InflowDetailView.as_view(), name='inflow_detail'),
+
+    path('api/v1/inflows/', views.InflowListCreateAPIView.as_view(), name='inflow-list-create-api-view'),
+    path('api/v1/inflows/<int:pk>/', views.InflowRetrieveUpdateDestroyAPIView.as_view(), name='inflow-detail-api-view'),
 ]
